05ab787d68e91ac36c5c23ab5412f04d708710d5
eleonora guidi6 <eleonora.guidi6@studio.unibo.it>
2015-05-12 16:06:17 +0200

    aggiunta classe basic frame

---
+++ src/it/unibo/tavernproj/view/BasicFrame.java
@@ -0,0 +1,29 @@
+package it.unibo.tavernproj.view;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class BasicFrame extends JFrame{
+  
+  private static final long serialVersionUID = 1L;
+  private final IGUIutilities util = new GUIutilities();
+  
+  /**
+   * Builds a new basic form with width and height half of the main view.
+   */
+  public BasicFrame() {
+    super();
+    setLocationByPlatform(true);
+    this.setSize(util.getDefaultWidth() / 2, util.getDefaultHeight() / 2);
+    this.setResizable(true);
+    this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    buildLayout();
+    this.setVisible(true);
+  }
+
+  private void buildLayout() {
+    this.getContentPane().setLayout(new BorderLayout());  }
+
+}
